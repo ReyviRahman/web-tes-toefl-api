384bf263625ec0cda68f9f6d6d6fcb7a98afd328
Reyvi Rahman <reyvirahman@gmail.com>
2024-12-02 23:29:12 +0700

    adding dotenv required in index

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+require("dotenv").config();
 const port = process.env.PORT || 3001;
 
 const sequelize = require('./db.config');
